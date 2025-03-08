29eb62ca096811287f621847971c3c97ee177545
Arseniy Mokin <a.mokin@smartengines.com>
2025-03-08 21:34:14 +0300

    add: added some new architectures

---
+++ metrics.py
@@ -0,0 +1,23 @@
+import numpy as np
+import torch.nn.functional as F
+from sklearn.metrics import roc_curve
+
+def compute_eer(label, pred, positive_label=1):
+    # all fpr, tpr, fnr, fnr, threshold are lists (in the format of np.array)
+    fpr, tpr, threshold = roc_curve(label, pred)
+    fnr = 1 - tpr
+
+    # the threshold of fnr == fpr
+    eer_threshold = threshold[np.nanargmin(np.absolute((fnr - fpr)))]
+
+    # theoretically eer from fpr and eer from fnr should be identical but they can be slightly differ in reality
+    eer_1 = fpr[np.nanargmin(np.absolute((fnr - fpr)))]
+    eer_2 = fnr[np.nanargmin(np.absolute((fnr - fpr)))]
+
+    # return the mean of eer from fpr and from fnr
+    eer = (eer_1 + eer_2) / 2
+    return eer
+
+def compute_similarity(embed1, embed2):
+    return F.cosine_similarity(embed1, embed2, dim=1).detach().cpu().numpy().tolist()
+    
